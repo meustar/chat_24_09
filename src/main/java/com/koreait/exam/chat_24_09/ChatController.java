b7bc6483e18bef976ce0b0fd7b98b5d3cd95b0f1
meustar <meustar829@gmail.com>
2024-09-06 15:29:38 +0900

    message filtering

--- src/main/java/com/koreait/exam/chat_24_09/ChatController.java
+++ src/main/java/com/koreait/exam/chat_24_09/ChatController.java
@@ -2,14 +2,17 @@ package com.koreait.exam.chat_24_09;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.IntStream;
 
 @Controller
 @RequestMapping("/chat")
+@Slf4j
 public class ChatController {
 
     private List<ChatMessage> chatMessages = new ArrayList<>();
@@ -28,8 +31,6 @@ public class ChatController {
     public record writeMessageResponse(long id) {
     }
 
-    public record messagesResponse(List<ChatMessage> chatMessages, long count) {
-    }
 
     @PostMapping("/writeMessage")
     @ResponseBody
@@ -42,10 +43,36 @@ public class ChatController {
         return new RsData<>("S-1", "메세지가 작성되었습니다.", new writeMessageResponse(message.getId()));
     }
 
+
+
+    public record messagesRequest(Long fromId) {
+    }
+
+    public record messagesResponse(List<ChatMessage> chatMessages, long count) {
+    }
+
     @GetMapping("/message")
     @ResponseBody
-    public RsData<messagesResponse> message() {
+    public RsData<messagesResponse> message(messagesRequest req) {
 
-        return new RsData<>("S-1", "메세지가 작성되었습니다.", new messagesResponse(chatMessages, chatMessages.size()));
+        List<ChatMessage> messages = chatMessages;
+
+        log.debug("req : {}", req);
+
+        // 번호가 같이 입력 되었다면?
+        if (req.fromId != null) {           // 글을 몇번째에 작성 했는지?
+            // 해당 번호의 채팅 메세지가 전체 리스트의 몇번째 인덱스인지? 체크. 없다면 -1.
+            int index = IntStream.range(0, messages.size())
+                    .filter(i -> chatMessages.get(i).getId() == req.fromId) //filter 거름망, 원하는것만 거르는 느낌
+                    .findFirst()             // 있다면 (여기까지)
+                    .orElse(-1);    // 없다면.
+            if (index != -1) {              // 글이 있다면.
+                // 만약에 index가 -1이 아니라면? 0번부터 index번 까지 제거한 리스트를 만든다.
+                messages = messages.subList(index + 1, messages.size());
+            }
+        }
+
+
+        return new RsData<>("S-1", "성공", new messagesResponse(messages, messages.size()));
     }
 }
